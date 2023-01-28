01e14d499eaa19641dfc282e9c0256afe4ed01ab
liulifox233 <88608708+liulifox233@users.noreply.github.com>
2023-01-28 13:55:44 +0800

    Update omofun.py

--- omofun.py
+++ omofun.py
@@ -19,10 +19,11 @@ def get_url(id,episodes=0):
     detail_url  = 'http://103.91.210.141:2515/xgapp.php/v2/video_detail'
     response_url = ((((((requests.get(detail_url,headers=head,params={'id':id})).json())['data'])['vod_info'])['vod_url_with_player'])[0])['url']
     video_url = ((((((requests.get(detail_url,headers=head,params={'id':id})).json())['data'])['vod_info'])['vod_url_with_player'])[0])['parse_api'].split('?')[0]
+    video_url = 'http://103.91.210.141:6100/jx.php'
     split_url = response_url.split('#')
     for i in range(len(split_url)):
         print(i,(split_url[i]).split('$')[0])
     episodes = int(input('请输入集数:'))
-    true_url = ((requests.get(video_url,params={'url':(split_url[episodes])})).json())['url']
+    true_url = ((requests.get(video_url,params={'url':(split_url[episodes].split('$')[1])})).json())['url']
     return true_url
 print(get_url(get_anime_id(input('请输入要搜索的番剧:'))))
